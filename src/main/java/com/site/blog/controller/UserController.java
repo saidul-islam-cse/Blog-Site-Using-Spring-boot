3fa25ed87a1c1523f2eb57263664bf8ace5cd5b4
saidulvai <saidulvaicsebgctub@gmail.com>
2026-09-12 19:01:34 +0600

    User Controller Complete

--- src/main/java/com/site/blog/controller/UserController.java
+++ src/main/java/com/site/blog/controller/UserController.java
@@ -4,6 +4,9 @@ import com.site.blog.entity.User;
 import com.site.blog.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -17,5 +20,25 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getALlUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user){
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
     }
 }
